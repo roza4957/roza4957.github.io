a84fb2db56fae70bc7daf10e1729aa589c73c63f
salmoooon <chriskevin1123@gmail.com>
2026-02-09 22:49:04 +0800

    Did a couple of things 1. Finishing the background 2. Added some translation keys 3. Resized/reordered the social icons to fit mobile screen

--- translations.js
+++ translations.js
@@ -1,5 +1,8 @@
 const translations = {
   en: {
+    social_video: "Video",
+    social_gallery: "Gallery",
+    social_community: "Community",
     enjoy_summary: "789",
     hate_summary: "123",
     gallery_title: "Gallery",
@@ -8,9 +11,20 @@ const translations = {
     contact_title: "Contact Me",
     contact_desc: "contact me info",
     contact_email: "Email",
-    footer: "© 2026 Omurice炸黑輪<br>Site made by salmoooon"
+    footer: "© 2026 Omurice炸黑輪<br>Site made by salmoooon, with the help of Github Copilot."
   },
   zh: {
-
+    social_video: "影音",
+    social_gallery: "作品集",
+    social_community: "社群",
+    enjoy_summary: "789",
+    hate_summary: "123",
+    gallery_title: "Gallery",
+    char_details_summary: "Details:",
+    show_more: "Show more (WIP)",
+    contact_title: "Contact Me",
+    contact_desc: "contact me info",
+    contact_email: "Email",
+    footer: "© 2026 Omurice炸黑輪<br>Site made by salmoooon, with the help of Github Copilot."
   }
 };
